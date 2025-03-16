a8fd7dc490e2b6f06d6e0403c4d25cc2789f160c
23jammy <23jammycruz@gmail.com>
2025-03-17 01:10:45 +0800

    Fix dashboard for receipts

--- public/user/index.php
+++ public/user/index.php
@@ -14,7 +14,7 @@
   <script src=" https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
-<body class="bg-[#d9d9d9]">
+<body class="bg-[#d9d9d9] flex flex-col w-screen min-h-screen overflow-x-hidden">
   <?php
   include_once '../includes/partial.php';
   include_once '../includes/connect-db.php';
@@ -32,7 +32,7 @@
 
 
   <!--Receipts-->
-  <div class="flex flex-wrap justify-center mt-3">
+  <div class="flex flex-col w-full h-full items-center mt-3 p-4">
     <?php
     $stmt = $pdo->prepare("
         SELECT 
@@ -41,6 +41,7 @@
           quantity,
           name,
           value,
+          idinventory,
           CASE 
           WHEN payment_type = 0 THEN 'Cash'
               WHEN payment_type = 1 THEN 'Gcash'
@@ -55,49 +56,50 @@
     $purchases = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     ?>
+    <!-- Receipts Grid -->
 
-    <?php foreach ($purchases as $purchase): ?>
-      <div class="clickable-div m-4 max-w-sm cursor-pointer" data-reference="<?= $purchase['reference_no'] ?>"
-        data-date="<?= $purchase['date'] ?>" data-quantity="<?= $purchase['quantity'] ?>"
-        data-item="<?= $purchase['name'] ?>" data-amount="<?= $purchase['value'] ?>"
-        data-mode="<?= $purchase['payment_type'] ?>">
+    <div class="w-full p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:w-10/12 gap-4 place-items-center">
+      <?php foreach ($purchases as $purchase): ?>
+        <div class="clickable-div w-[16rem] cursor-pointer"   data-reference="<?= $purchase['reference_no'] ?>"
+          data-date="<?= $purchase['date'] ?>" data-quantity="<?= $purchase['quantity'] ?>"
+          data-item="<?= $purchase['name'] ?>" data-amount="<?= $purchase['value'] ?>"
+          data-inventory="<?= $purchase['idinventory'] ?>"
+          data-mode="<?= $purchase['payment_type'] ?>">
 
-        <div class="flex rounded-lg h-full bg-white p-8 flex-col hover:shadow-2xl">
-          <div class="flex items-center mb-2 pb-3 border-b-1 border-black">
-            <h2 class="text-black text-lg font-bold mr-1">PAYMENT RECEIPT</h2>
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center flex-shrink-0 ml-auto">
-              <svg class="w-6 h-6 text-black mt-3 text-blackinline-flex items-center" aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 18">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                  d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3" />
-              </svg>
+          <div class="flex rounded-lg h-full bg-white p-6 flex-col hover:shadow-2xl">
+            <div class="flex items-center mb-2 pb-3 border-b-1 border-black">
+              <h2 class="text-black text-lg font-bold mr-1">PAYMENT RECEIPT</h2>
+              <div class="inline-flex items-center justify-end flex-shrink-0 ml-auto">
+                <i id="downloadIcon" onclick="downloadReceipt(this)" class="fa-solid fa-download hover:text-gray-700"></i>
+              </div>
             </div>
-          </div>
 
-          <div class="pb-3 border-b border-black text-black">
-            <div class="grid grid-cols-2 gap-x-4">
-              <p>Date:</p>
-              <p class="text-right font-bold"><?= $purchase['date'] ?></p>
+            <div class="pb-3 border-b border-black text-black">
+              <div class="grid grid-cols-2 gap-x-4">
+                <p>Date:</p>
+                <p class="text-right font-bold"><?= $purchase['date'] ?></p>
 
-              <p>Item:</p>
-              <p class="text-right font-bold"><?= $purchase['name'] ?></p>
+                <p>Item:</p>
+                <p class="text-right font-bold"><?= $purchase['name'] ?></p>
 
-              <p>Mode:</p>
-              <p class="text-right font-bold"><?= $purchase['payment_type'] ?></p>
+                <p>Mode:</p>
+                <p class="text-right font-bold"><?= $purchase['payment_type'] ?></p>
 
-              <p>Paid:</p>
-              <p class="text-right font-bold">P <?= $purchase['value'] ?></p>
+                <p>Paid:</p>
+                <p class="text-right font-bold">P <?= $purchase['value'] ?></p>
+              </div>
             </div>
-          </div>
 
-          <div class="flex flex-col justify-between flex-grow text-center">
-            <p class="leading-relaxed text-base text-gray-800 my-3 font-light">
-              Tap to see full details
-            </p>
+            <div class="flex flex-col justify-between flex-grow text-center">
+              <p class="leading-relaxed text-base text-gray-800 my-3 font-light">
+                Tap to see full details
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    <?php endforeach; ?>
+      <?php endforeach; ?>
+    </div>
+
 
   </div>
 
@@ -127,15 +129,15 @@
             Association of Computer Scientists
           </p>
         </div>
-        <div class="flex justify-center items-center pb-2 border-b-1 border-gray-400">
+        <div class="flex justify-center items-center pb-2">
           <p class="text-2xl font-bold mt-1">PAYMENT RECEIPT</p>
+        </div>
+        <div class="flex py-1 gap-2 w-full border-b-1 border-gray-400">
+          <p class="text-xs font-bold text-gray-600">REF</p>
+          <p id="reference" class="text-xs font-bold">131231231231</p>
         </div>
         <div class="border-b border-gray-400 text-black pt-1 pb-1">
           <div class="grid grid-cols-2 gap-x-4">
-            <p class="font-semibold text-gray-400">Reference no.</p>
-            <p id="reference" class="font-semibold">131231231231</p>
-
-
             <p class="font-semibold text-gray-400">Date</p>
             <p id="date" class="font-semibold">01/01/2025</p>
 
@@ -166,7 +168,7 @@
             <p id="total" class="font-bold">P 100.00</p>
           </div>
         </div>
-        <p class="pt-1 pb-1">This is a cutomer's copy. Thank You!</p>
+        <p class="pt-1 pb-1 text-center">This is a cutomer's copy. Thank You!</p>
         <div class="mt-4 flex justify-center">
           <button id="downloadButton"
             class="px-20 bg-black p-3 ml-3 rounded-lg text-white hover:bg-white border-1 hover:border-1 hover:text-black hover:shadow-2xl cursor-pointer">
@@ -187,6 +189,14 @@
           },
         });
       });
+
+    function downloadReceipt(element) {
+      html2canvas(document.querySelector(".modal-container"), {
+          onrendered: function (canvas) {
+            // document.body.appendChild(canvas);
+            return Canvas2Image.saveAsPNG(canvas);
+          }, });
+    }
 
     $(document).ready(function () {
       $(document).on("click", ".clickable-div", function () {
