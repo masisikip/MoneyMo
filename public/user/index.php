a2e02b76d43327b734c5b1e303cc37be2c1ffa4e
JovTim <202280218@psu.palawan.edu.ph>
2025-09-20 10:03:53 +0800

    [adjustment] adjusted the size of modal receipt

--- public/user/index.php
+++ public/user/index.php
@@ -12,6 +12,7 @@
   <link rel="icon" href="./assets/favicon.ico" type="image/x-icon" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
   <script src=" https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 
 <body class="bg-[#d9d9d9] flex flex-col w-screen min-h-screen overflow-x-hidden">
@@ -105,56 +106,68 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
 
   <!-- Receipt Modal -->
-  <div id="myModal" class="modal fixed w-full h-full top-0 left-0 hidden items-center justify-center z-20">
+  <div
+    id="myModal"
+    class="modal fixed w-full h-full top-0 left-0 hidden items-center justify-center z-20">
     <!-- hoverlay  -->
-    <div id="modalOverlay" class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+    <div
+      id="modalOverlay"
+      class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
 
     <div
       class="modal-container flex flex-col items-center h-auto bg-white w-11/12 md:max-w-md my-10 mx-auto shadow-lg z-50 overflow-y-auto">
       <div class="bg-black w-full grid grid-cols-2 gap-x-4">
         <p class="text-white font-medium ml-3 p-2">MoneyMo</p>
-        <button type="button" id="closeModal"
+        <button
+          type="button"
+          id="closeModal"
           class="text-white hover:bg-white hover:text-black px-4 py-2 ml-auto cursor-pointer">
           X
         </button>
       </div>
       <!-- Add modal content here -->
-      <div class="w-3/4 modal-content py-4 text-left px-6 bg-white">
+      <div class="w-3/4 modal-content py-4 text-left px-4 md:px-6 bg-white">
         <div class="flex justify-center items-center">
-          <img src="../assets/acs_logo.jpg" alt="@" class="h-16 m-2">
+          <img src="../assets/acs_logo.jpg" alt="@" class="h-16 m-2" />
         </div>
-        <div class="flex justify-center items-center">
+        <div class="flex justify-center items-center text-center">
           <p class="text-1 font-extralight">
             Association of Computer Scientists
           </p>
         </div>
-        <div class="flex justify-center items-center pb-2">
-          <p class="text-2xl font-bold mt-1">PAYMENT RECEIPT</p>
+        <div class="flex justify-center items-center text-center py-2">
+          <p class="text-xl md:text-2xl font-bold mt-1">PAYMENT RECEIPT</p>
         </div>
         <div class="py-1 gap-2 w-full border-b-1 border-gray-400">
-          <p class="text-xs font-bold text-gray-600">Ref no. <span id="reference" class="text-xs font-bold">131231231231</span></p>
-          <p class="text-xs font-bold text-gray-600">Ctrl no. <span id="ctrl" class="text-xs font-bold">131231231231</span></p>
-
+          <p class="text-xs font-bold text-gray-600">
+            Ref no.
+            <span id="reference" class="text-xs font-bold">131231231231</span>
+          </p>
+          <p class="text-xs font-bold text-gray-600">
+            Ctrl no.
+            <span id="ctrl" class="text-xs font-bold">131231231231</span>
+          </p>
         </div>
         <div class="border-b border-gray-400 text-black pt-1 pb-1">
           <div class="grid grid-cols-2 gap-x-4">
-            <p class="font-semibold text-gray-400">Date</p>
+            <p class="font-semibold text-gray-400 text-sm md:text-base">Date</p>
             <p id="date" class="font-semibold">01/01/2025</p>
 
-            <p class="font-semibold text-gray-400">Payment Method</p>
+            <p class="font-semibold text-gray-400 text-sm md:text-base">Payment Method</p>
             <p id="mode" class="font-semibold">Gcash</p>
 
-            <p class="font-semibold text-gray-400">Item</p>
-            <p id="item" class="font-semibold">Org Fee</p>
+            <p class="font-semibold text-gray-400 text-sm md:text-base">Item</p>
+            <p id="item" class="font-semibold leading-tight">
+              Organization Shirt Small
+            </p>
 
-            <p class="font-semibold text-gray-400">Quantity</p>
+            <p class="font-semibold text-gray-400 text-sm md:text-base">Quantity</p>
             <p id="quantity" class="font-semibold">ACS</p>
 
-            <p class="font-semibold text-gray-400">Product Price</p>
+            <p class="font-semibold text-gray-400 text-sm md:text-base">Product Price</p>
             <p id="amount" class="font-semibold">₱100.00</p>
           </div>
         </div>
-
 
         <div class="border-b border-gray-400 text-black pt-1 pb-1">
           <div class="grid grid-cols-2 gap-x-4">
@@ -162,10 +175,13 @@
             <p id="total" class="font-bold">₱ 100.00</p>
           </div>
         </div>
-        <p class="pt-1 pb-1 text-center mt-5">This is a cutomer's copy. Thank You!</p>
+        <p class="pt-4 text-center text-xs">
+          This is a cutomer's copy. Thank You!
+        </p>
       </div>
-      <div class="my-4 flex justify-center">
-        <button id="downloadButton"
+      <div class="my-2 flex justify-center">
+        <button
+          id="downloadButton"
           class="px-20 bg-black p-3 ml-3 rounded-lg text-white hover:bg-white border-1 hover:border-1 hover:text-black hover:shadow-2xl cursor-pointer">
           Download
         </button>
@@ -236,11 +252,13 @@
       // Show modal when button is clicked
       $(".clickable-div").click(function() {
         $("#myModal").removeClass("hidden").addClass("flex");
+        $("body").addClass("overflow-hidden");
       });
 
       // Hide modal when close button is clicked
       $("#closeModal").click(function() {
         $("#myModal").addClass("hidden").removeClass("flex");
+        $("body").removeClass("overflow-hidden");
       });
     });
 
