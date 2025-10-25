6fcc0fa25b6462bd01a49e068c71c4e9e4897344
23jammy <23jammycruz@gmail.com>
2025-10-25 14:47:48 +0800

    Changes added in id verification

--- public/admin/scanner/index.php
+++ public/admin/scanner/index.php
@@ -28,14 +28,19 @@ try {
     <main class="w-full h-full flex flex-col items-center min-h-screen">
         <div class="font-bold text-2xl mt-5">Scan QR</div>
         <!-- QR Code Scanner -->
-        <div id="qr-reader" class="mt-5 w-[23rem] border-4 border-transparent transition-colors"></div>
+        <div id="qr-reader" class="mt-5 w-92 border-4 border-transparent transition-colors"></div>
+        <!-- Manual Type Button -->
+        <button id="manual_btn" type="button" onclick="openManualType()"
+            class="mt-10 w-92 rounded p-2 text-center text-white bg-black hover:bg-gray-800 cursor-pointer">
+            Manual Input
+        </button>
 
     </main>
 
     <!-- Items section -->
     <div id="items" class="fixed w-full h-full bg-gray-500/50 top-0 left-0 justify-center items-center hidden">
         <div id="items-main"
-            class="mt-4 w-[20rem] md:w-[30rem] h-[30rem] rounded-md flex flex-col bg-white p-4 overflow-y-auto items-center">
+            class="mt-4 w-[20rem] md:w-120 h-120 rounded-md flex flex-col bg-white p-4 overflow-y-auto items-center">
             <div class="w-full items-center flex justify-between">
                 <span class="text-xl">Select Items to Avail</span>
                 <div class="flex items-center px-1">
@@ -87,10 +92,51 @@ try {
                 </div>
                 <input type="hidden" id="student_id" name="student_id">
                 <button onclick="processPayment(event)"
-                    class="w-20 p-1 bg-black rounded-md text-white cursor-pointer hover:bg-zinc-700 mt-10">Confirm</button>
+                    class="w-1/3 p-1 bg-black rounded text-white cursor-pointer hover:bg-zinc-700 mt-10">Confirm</button>
             </form>
         </div>
     </div>
+
+    <!-- Manual Type Modal -->
+
+    <div id="manual_type_modal"
+        class="w-full h-full fixed top-0 left-0 bg-black/40 backdrop-blur-sm justify-center items-center hidden">
+        <div class="rounded bg-white p-4 flex flex-col h-48 w-[20rem]">
+            <div class="text-xl font-bold w-full text-center mb-4">Manual ID Input</div>
+            <label for="student_id_manual" class="font-semibold">Enter Student ID</label>
+            <div class="mt-1 w-full">
+                <input type="text" id="student_id_manual" name="student_id" placeholder="20XX-X-XXXX"
+                    class="p-1 border rounded focus:outline-none w-full">
+            </div>
+            <div class="w-full flex justify-center mt-auto">
+                <button type="button" onclick="submitID()"
+                    class="w-1/3 bg-black p-1 rounded hover:bg-gray-800 cursor-pointer text-white text-center">Submit</button>
+                <button type="button" onclick="closeManualType()" id="cancel_btn"
+                    class="w-1/3 bg-gray-600 p-1 rounded hover:bg-gray-500 cursor-pointer text-white text-center ml-2">Cancel</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Error User Modal -->
+    <div id="error_user_modal"
+        class="fixed inset-0 bg-black/40 backdrop-blur-sm items-center justify-center hidden">
+        <div class="bg-white rounded-lg p-6 w-80 flex flex-col items-center shadow-lg">
+            <div class="text-red-500 text-6xl mb-3">
+                <i class="fa-regular fa-circle-xmark"></i>
+            </div>
+            <h2 class="text-2xl font-semibold text-gray-800 mb-1 text-center">
+                Invalid User!
+            </h2>
+            <p class="text-gray-500 text-sm text-center mb-4">
+                Please check student ID and try again.
+            </p>
+            <button type="button" onclick="closeErrorUser()"
+                class="bg-red-500 hover:bg-red-600 text-white font-medium px-5 py-2 rounded-md cursor-pointer">
+                Close
+            </button>
+        </div>
+    </div>
+
 
     <!-- Screen Loader -->
     <div id="loader" class="w-full h-full fixed items-center justify-center top-0 left-0 bg-gray-700/50 hidden">
@@ -103,6 +149,9 @@ try {
 </html>
 
 <script>
+    let qrScanner;
+    let selectedCameraId = null;
+
     function selectItem(element) {
         let item = $(element);
         let checkbox = item.find('input[name="iditem[]"]');
@@ -122,7 +171,6 @@ try {
         event.preventDefault();
 
         let form = new FormData($('#item-form')[0]);
-
         $('#loader').removeClass('hidden').addClass('flex');
 
         $.ajax({
@@ -137,6 +185,7 @@ try {
                 $('body').append(response);
                 $('#item-form')[0].reset();
                 $('.item-card').addClass('border-gray-300').removeClass('border-green-300');
+                startCamera();
             },
             error: function (xhr, status, error) {
                 $('#loader').addClass('hidden').removeClass('flex');
@@ -147,12 +196,133 @@ try {
         });
     }
 
+    function closeErrorUser() {
+        $("#error_user_modal").addClass("hidden").removeClass("flex");
+        startCamera();
+    }
+
+    function openManualType() {
+        $("#manual_type_modal").removeClass("hidden").addClass("flex");
+        if (qrScanner) {
+            qrScanner.stop()
+                .then(() => console.log("Camera stopped because manual input modal opened."))
+                .catch(err => console.warn("Error stopping camera:", err));
+        }
+    }
+
+    function closeManualType() {
+        $("#manual_type_modal").removeClass("flex").addClass("hidden");
+        $("#student_id_manual").val('');
+    }
+
+    function submitID() {
+        let student_id;
+        if ($("#student_id").val() === "") {
+            student_id = $("#student_id_manual").val();
+            $("#student_id").val(student_id);
+        } else {
+            student_id = $("#student_id").val();
+        }
+
+        $.ajax({
+            url: "logic/id_check.php",
+            method: 'POST',
+            data: {
+                student_id: student_id
+            },
+            processData: false,
+            contentType: false,
+            dataType: "json",
+            success: function (response) {
+                $('#loader').addClass('hidden').removeClass('flex');
+                if (response.status == "success") {
+                    $('#items').removeClass('hidden').addClass('flex');
+                    console.log(response.status)
+                } else if (response.status == "error") {
+                    $("#error_user_modal").removeClass("hidden").addClass("flex");
+                    console.log(response.status)
+                }
+            },
+            error: function (xhr, status, error) {
+                $('#loader').addClass('hidden').removeClass('flex');
+                console.error("AJAX Error: ", error);
+                console.error("Status: ", status);
+                console.error("Response Text: ", xhr.responseText);
+                $("#error_user_modal").removeClass("hidden").addClass("flex");
+            }
+        });
+
+        $("#manual_type_modal").removeClass("flex").addClass("hidden");
+        $("#student_id_manual").val('');
+    }
+
+    function onScanSuccess(decodedText, decodedResult) {
+        let data = decodedText;
+        if (decodedText.includes("~")) {
+            let qrData = decodedText.split("~");
+            data = qrData[0].trim();
+        }
+
+        $("#student_id").val(data);
+
+        $("#qr-reader")
+            .addClass("border-green-500")
+            .removeClass("border-transparent");
+        setTimeout(() => {
+            $("#qr-reader")
+                .removeClass("border-green-500")
+                .addClass("border-transparent");
+        }, 500);
+
+        // $("#items").removeClass("hidden").addClass("flex");
+        submitID();
+        qrScanner.stop()
+            .then(() => console.log("Camera stopped because manual input modal opened."))
+            .catch(err => console.warn("Error stopping camera:", err));
+    }
+
+    function onScanFailure(error) {
+        // This runs when no QR is detected — you can safely leave it empty
+        // console.warn("Scan failed:", error);
+    }
+
+    // 🔹 Handles camera selection and startup
+    function startCamera() {
+        Html5Qrcode.getCameras()
+            .then(cameras => {
+                if (cameras.length === 0) {
+                    alert("No camera found.");
+                    return;
+                }
+
+                const backCamera = cameras.find(cam =>
+                    cam.label.toLowerCase().includes("back") ||
+                    cam.label.toLowerCase().includes("rear")
+                );
+                const frontCamera = cameras.find(cam =>
+                    cam.label.toLowerCase().includes("front") ||
+                    cam.label.toLowerCase().includes("user")
+                );
+
+                const selectedCamera = backCamera || frontCamera || cameras[0];
+                selectedCameraId = selectedCamera.id;
+
+                return qrScanner.start(
+                    selectedCameraId,
+                    { fps: 10, qrbox: { width: 250, height: 250 } },
+                    onScanSuccess,
+                    onScanFailure
+                );
+            })
+            .then(() => console.log("Camera started successfully."))
+            .catch(err => alert("Error starting camera: " + err));
+    }
+
     $(document).ready(function () {
         $('#header-title').text('QR Scan');
 
         $('#select-all').on('change', function () {
             let checked = $(this).prop('checked');
-
             $('.item-card').each(function () {
                 if ($(this).data('stock') <= 0) return;
 
@@ -170,50 +340,22 @@ try {
         });
 
         const qrReader = $("#qr-reader");
-        function onScanSuccess(decodedText) {
-            let data = decodedText;
-            if (decodedText.length > 15) {
-                let qrData = decodedText.split(' ~ ');
 
-                data = qrData[0].trim();
-            }
-            $('#student_id').val(data);
-            qrReader.addClass("border-green-500").removeClass('border-transparent');
-            setTimeout(() => qrReader.removeClass("border-green-500").addClass('border-transparent'), 500);
-            $('#items').removeClass('hidden').addClass('flex');
-        }
+        qrScanner = new Html5Qrcode("qr-reader");
 
-        function onScanFailure() {
-            return;
-        }
+        startCamera();
 
-        const qrScanner = new Html5Qrcode("qr-reader");
-
-        Html5Qrcode.getCameras().then(function (cameras) {
-            if (cameras.length > 0) {
-                let backCamera = cameras.find(cam => cam.label.toLowerCase().includes('back') || cam.label.toLowerCase().includes('rear'));
-                let frontCamera = cameras.find(cam => cam.label.toLowerCase().includes('front') || cam.label.toLowerCase().includes('user'));
-
-                let selectedCamera = backCamera || frontCamera || cameras[0];
-
-                qrScanner.start(selectedCamera.id, { fps: 10, qrbox: { width: 250, height: 250 } }, onScanSuccess, onScanFailure)
-                    .then(() => console.log("Camera started successfully:", selectedCamera.label))
-                    .catch(err => alert("Error starting camera: " + err));
-            } else {
-                alert("No camera found.");
-            }
-        }).catch(function (err) {
-            alert("Error accessing cameras: " + err);
+        $("#cancel_btn").on("click", function () {
+            startCamera();
         });
-
 
         $(document).on('click', function (event) {
             if (!$(event.target).closest('#items-main').length && $(event.target).closest('#items').length) {
                 $('#items').addClass('hidden').removeClass('flex');
                 $('#item-form')[0].reset();
                 $('.item-card').addClass('border-gray-300').removeClass('border-black');
+                startCamera();
             }
-        })
-
-    })
+        });
+    });
 </script>
